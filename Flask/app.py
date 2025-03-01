3ec244c09566c1cf0ee131e1006b127114cf27ef
ryanjrt <tham.jurong@gmail.com>
2025-03-01 17:50:05 +0800

    Flask tutorial VScode

---
+++ Flask/app.py
@@ -0,0 +1,40 @@
+import re
+from datetime import datetime
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+# @app.route("/hello/<name>")
+# def hello_there(name):
+#     now = datetime.now()
+#     formatted_now = now.strftime("%A, %d %b, %Y at %X")
+    
+#     # Filter the name argument to letters only using regular expressions. URL arguments
+#     # can contain arbitrary text, so we restrict to safe characters only.
+#     match_object = re.match("[a-zA-Z]+", name)
+    
+#     if match_object:
+#         clean_name = match_object.group(0)
+#     else:
+#         clean_name = "Friend"
+        
+#     content = "Hello there, " + clean_name + "! Its " + formatted_now
+#     return content
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
+    
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
